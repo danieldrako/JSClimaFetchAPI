c5fa9b1c0c8262afcb5206916ca25c56298ae775
danieldrako <ls.ortizdaniel@gmail.com>
2022-11-24 22:37:09 -0600

    after validating form

--- js/app.js
+++ js/app.js
@@ -12,6 +12,45 @@ function buscarClima(e){
     e.preventDefault();
 
     //Validar 
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad ==='' || pais === ''){
+        //Hubo error
+        mostrarError('Ambos campos son obligatorios');
+
+        return;
+    }
+
+
 
     //Consultar API
 }
+
+function mostrarError(mensaje){
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta){
+        // Crear alerta
+        const alerta = document.createElement('div');
+
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+        
+        alerta.innerHTML =`
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${mensaje}</span>
+        `;
+
+        container.appendChild(alerta);
+
+        //Se elimine la alerta despues de 5 s
+        setTimeout(() => {
+            alerta.remove();
+        },5000);
+    }
+
+    
+}
+
+
+
